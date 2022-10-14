2f377911e3998b3290cea31728d53f34de7a7e72
Алёна Чистякова <113229355+aspohdl@users.noreply.github.com>
2022-10-14 12:35:46 +0300

    Update 119.py

--- 119.py
+++ 119.py
@@ -3,11 +3,13 @@ k1 = 0
 count = 0
 for k1 in range(1, a+1):
     number = k1
-    flip = 0
+    b = 0
     while number > 0:
-        flip = flip * 10 + number % 10
+        k = number % 10
         number = number // 10
-    if k1 == flip:
+        b = b * 10
+        b += k
+    if k1 == b:
        count += 1
 
 print(count)
