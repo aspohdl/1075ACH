f78755e63ae22843dc0cee29131fa4cc7594e062
Алёна Чистякова <113229355+aspohdl@users.noreply.github.com>
2023-02-12 15:56:47 +0300

    Add files via upload

---
+++ pathetic.cs
@@ -0,0 +1,23 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 10)
+{
+    if (number <= 99)
+    {
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"Число {number} чётное и двузначное");
+        }
+        else if (number % 2 == 1)
+        {
+            Console.WriteLine($"Число {number} НЕ является чётным");
+        }
+    }
+    else if (number > 99)
+    {
+        Console.WriteLine($"Число {number} НЕ является двузначным");
+    }
+}
+else if (number < 10)
+{
+    Console.WriteLine($"Число {number} НЕ является двузначным");
+}
